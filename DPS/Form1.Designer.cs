e49920f2578db80fcfefb1b644ec284cb66016fb
mluzarow <mluzarow@purdue.edu>
2016-07-05 22:34:30 -0500

    init

---
+++ DPS/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace DPS {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose (bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent () {
+            this.txt_dmg = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_critDmg = new System.Windows.Forms.TextBox();
+            this.txt_chance = new System.Windows.Forms.TextBox();
+            this.txt_rate = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_calc = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lbl_est = new System.Windows.Forms.Label();
+            this.lbl_crit = new System.Windows.Forms.Label();
+            this.lbl_raw = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_load = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txt_dmg
+            // 
+            this.txt_dmg.BackColor = System.Drawing.SystemColors.Window;
+            this.txt_dmg.Location = new System.Drawing.Point(86, 16);
+            this.txt_dmg.MaxLength = 100;
+            this.txt_dmg.Name = "txt_dmg";
+            this.txt_dmg.Size = new System.Drawing.Size(100, 20);
+            this.txt_dmg.TabIndex = 0;
+            this.txt_dmg.TextChanged += new System.EventHandler(this.txt_dmg_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_critDmg);
+            this.groupBox1.Controls.Add(this.txt_chance);
+            this.groupBox1.Controls.Add(this.txt_rate);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txt_dmg);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(204, 109);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input your values";
+            // 
+            // txt_critDmg
+            // 
+            this.txt_critDmg.Location = new System.Drawing.Point(86, 76);
+            this.txt_critDmg.Name = "txt_critDmg";
+            this.txt_critDmg.Size = new System.Drawing.Size(100, 20);
+            this.txt_critDmg.TabIndex = 3;
+            this.txt_critDmg.TextChanged += new System.EventHandler(this.txt_critDmg_TextChanged);
+            // 
+            // txt_chance
+            // 
+            this.txt_chance.Location = new System.Drawing.Point(86, 56);
+            this.txt_chance.Name = "txt_chance";
+            this.txt_chance.Size = new System.Drawing.Size(100, 20);
+            this.txt_chance.TabIndex = 2;
+            this.txt_chance.TextChanged += new System.EventHandler(this.txt_chance_TextChanged);
+            // 
+            // txt_rate
+            // 
+            this.txt_rate.Location = new System.Drawing.Point(86, 36);
+            this.txt_rate.Name = "txt_rate";
+            this.txt_rate.Size = new System.Drawing.Size(100, 20);
+            this.txt_rate.TabIndex = 1;
+            this.txt_rate.TextChanged += new System.EventHandler(this.txt_rate_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Crit Damg:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Chance:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Rate:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Damage:";
+            // 
+            // btn_calc
+            // 
+            this.btn_calc.Location = new System.Drawing.Point(22, 127);
+            this.btn_calc.Name = "btn_calc";
+            this.btn_calc.Size = new System.Drawing.Size(176, 71);
+            this.btn_calc.TabIndex = 4;
+            this.btn_calc.Text = "Calculate";
+            this.btn_calc.UseVisualStyleBackColor = true;
+            this.btn_calc.Click += new System.EventHandler(this.btn_calc_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Raw Damg:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lbl_est);
+            this.groupBox2.Controls.Add(this.lbl_crit);
+            this.groupBox2.Controls.Add(this.lbl_raw);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(222, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(221, 109);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Output";
+            // 
+            // lbl_est
+            // 
+            this.lbl_est.AutoSize = true;
+            this.lbl_est.Location = new System.Drawing.Point(104, 63);
+            this.lbl_est.Name = "lbl_est";
+            this.lbl_est.Size = new System.Drawing.Size(0, 13);
+            this.lbl_est.TabIndex = 9;
+            // 
+            // lbl_crit
+            // 
+            this.lbl_crit.AutoSize = true;
+            this.lbl_crit.Location = new System.Drawing.Point(104, 43);
+            this.lbl_crit.Name = "lbl_crit";
+            this.lbl_crit.Size = new System.Drawing.Size(0, 13);
+            this.lbl_crit.TabIndex = 8;
+            // 
+            // lbl_raw
+            // 
+            this.lbl_raw.AutoSize = true;
+            this.lbl_raw.Location = new System.Drawing.Point(104, 23);
+            this.lbl_raw.Name = "lbl_raw";
+            this.lbl_raw.Size = new System.Drawing.Size(0, 13);
+            this.lbl_raw.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(6, 56);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(76, 20);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Estimate:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(6, 36);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 20);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Pure Crit:";
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(222, 127);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(221, 32);
+            this.btn_save.TabIndex = 7;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_load
+            // 
+            this.btn_load.Location = new System.Drawing.Point(222, 166);
+            this.btn_load.Name = "btn_load";
+            this.btn_load.Size = new System.Drawing.Size(221, 32);
+            this.btn_load.TabIndex = 8;
+            this.btn_load.Text = "Load";
+            this.btn_load.UseVisualStyleBackColor = true;
+            this.btn_load.Click += new System.EventHandler(this.btn_load_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 220);
+            this.Controls.Add(this.btn_load);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btn_calc);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "DPS Calulator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_dmg;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_critDmg;
+        private System.Windows.Forms.TextBox txt_chance;
+        private System.Windows.Forms.TextBox txt_rate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_calc;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lbl_est;
+        private System.Windows.Forms.Label lbl_crit;
+        private System.Windows.Forms.Label lbl_raw;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_load;
+    }
+}
+
---
+++ DPS/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DPS {
+    public partial class Form1 : Form {
+        #region Structs
+        /// <summary>
+        /// Stores DPS data as a struct / class of floats.
+        /// </summary>
+        private class DPS_Data {
+            #region Public Fields
+            /// <summary>
+            /// The base damage on the target.
+            /// </summary>
+            public float damage;
+            /// <summary>
+            /// The attacks per second performed by the target.
+            /// </summary>
+            public float attackSpeed;
+            /// <summary>
+            /// The percentage chance the target has of getting a critical hit, per each hit.
+            /// </summary>
+            public float critChance;
+            /// <summary>
+            /// The critical damage of the target.
+            /// </summary>
+            public float critDamage;
+            /// <summary>
+            /// Calculated damage output with no critical hits.
+            /// </summary>
+            public float calcDamage;
+            /// <summary>
+            /// Calculated all critical damage output (theoretical maximum).
+            /// </summary>
+            public float calcCritDamage;
+            /// <summary>
+            /// Calculated estimate of damage target will do.
+            /// </summary>
+            public float calcEstimate;
+            #endregion Public Fields
+
+            #region Constructor
+            public DPS_Data (float damage, float attackSpeed, float critChance, float critDamage) {
+                this.damage = damage;
+                this.attackSpeed = attackSpeed;
+                this.critChance = critChance;
+                this.critDamage = critDamage;
+
+                CalculateDamage ();
+            }
+            public DPS_Data () {
+                // Ain't constructin' nothin'
+            }
+            #endregion Constructor
+
+            #region Public Methods
+            public void CalculateDamage () {
+                this.calcDamage = this.damage * this.attackSpeed;
+                this.calcCritDamage = this.damage * this.attackSpeed * this.critDamage;
+                this.calcEstimate = (this.damage * this.attackSpeed * (1 - this.critChance)) + (this.damage * this.attackSpeed * this.critChance * this.critDamage);
+            }
+            #endregion Public Methods
+
+        }
+        #endregion Structs
+
+
+        #region Private Variables
+        private DPS_Data dpsData = new DPS_Data ();
+        #endregion Private Variables
+
+
+        public Form1 () {
+            InitializeComponent();
+        }
+
+        private void printDPS () {
+            txt_dmg.Text = dpsData.damage.ToString ();
+            txt_rate.Text = dpsData.attackSpeed.ToString ();
+            txt_chance.Text = dpsData.critChance.ToString ();
+            txt_critDmg.Text = dpsData.critDamage.ToString ();
+            lbl_raw.Text = dpsData.calcDamage.ToString ();
+            lbl_crit.Text = dpsData.calcCritDamage.ToString ();
+            lbl_est.Text = dpsData.calcEstimate.ToString ();
+        }
+
+
+        #region Calculation
+        private void btn_calc_Click (object sender, EventArgs e) {
+            bool error = false;
+            // Attempt int parse of textbox text to floats
+
+            // If txt_dmg is not parseable
+            if (!float.TryParse(txt_dmg.Text, out dpsData.damage)) {
+                // Label the textbox as incorrect and return control
+                txt_dmg.BackColor = System.Drawing.Color.Red;
+                error = true;
+            }
+            if (!float.TryParse (txt_rate.Text, out dpsData.attackSpeed)) {
+                txt_rate.BackColor = System.Drawing.Color.Red;
+                error = true;
+            }
+            if (!float.TryParse(txt_chance.Text, out dpsData.critChance)) {
+                txt_chance.BackColor = System.Drawing.Color.Red;
+                error = true;
+            }
+            if (!float.TryParse(txt_critDmg.Text, out dpsData.critDamage)) {
+                txt_critDmg.BackColor = System.Drawing.Color.Red;
+                error = true;
+            }
+
+            if (!error) {
+                //Calc
+                dpsData.CalculateDamage ();
+                printDPS ();
+            }
+        }
+
+        private void txt_dmg_TextChanged (object sender, EventArgs e) {
+            if (txt_dmg.BackColor == System.Drawing.Color.Red) {
+                txt_dmg.BackColor = System.Drawing.Color.White;
+            }
+        }
+
+        private void txt_rate_TextChanged (object sender, EventArgs e) {
+            if (txt_rate.BackColor == System.Drawing.Color.Red) {
+                txt_rate.BackColor = System.Drawing.Color.White;
+            }
+        }
+
+        private void txt_chance_TextChanged (object sender, EventArgs e) {
+            if (txt_chance.BackColor == System.Drawing.Color.Red) {
+                txt_chance.BackColor = System.Drawing.Color.White;
+            }
+        }
+
+        private void txt_critDmg_TextChanged (object sender, EventArgs e) {
+            if (txt_critDmg.BackColor == System.Drawing.Color.Red) {
+                txt_critDmg.BackColor = System.Drawing.Color.White;
+            }
+        }
+        #endregion Calculation
+
+        #region IO
+        private void btn_save_Click (object sender, EventArgs e) {
+            Stream f = null;
+            StreamWriter fw = null;
+            SaveFileDialog fs = new SaveFileDialog ();
+
+            fs.InitialDirectory = Application.StartupPath + "\\Save Data\\";
+            fs.Filter = "DPS Meter files (*.dps)|*.dps|All files (*.*)|*.*";
+            fs.FilterIndex = 1;
+            fs.RestoreDirectory = true;
+
+            if (fs.ShowDialog () == DialogResult.OK) {
+                try {
+                    if ((f = fs.OpenFile()) != null) {
+                        using (fw = new StreamWriter(f)) {
+                            fw.WriteLine(dpsData.damage + "/" + dpsData.attackSpeed + "/" + dpsData.critChance + "/" + dpsData.critDamage + "/" + dpsData.calcDamage + "/" + dpsData.calcCritDamage + "/" + dpsData.calcEstimate);
+                        }
+                    }
+                } catch (Exception ex) {
+                    MessageBox.Show("Could not save file.\n" + ex.Message);
+                }
+            }
+        }
+
+        private void btn_load_Click (object sender, EventArgs e) {
+            Stream f = null;
+            StreamReader fr = null;
+            OpenFileDialog fd = new OpenFileDialog ();
+            string line = "";
+
+            fd.InitialDirectory = Application.StartupPath + "\\Save Data\\";
+            fd.Filter = "DPS Meter files (*.dps)|*.dps|All files (*.*)|*.*";
+            fd.FilterIndex = 1;
+            fd.RestoreDirectory = true;
+
+            if (fd.ShowDialog () == DialogResult.OK) {
+                try {
+                    if ((f = fd.OpenFile ()) != null) {
+                        using (fr = new StreamReader (f)) {
+                            line = fr.ReadLine ();
+
+                            string[] lineSplit = line.Split('/');
+                            dpsData.damage = Convert.ToSingle (lineSplit[0]);
+                            dpsData.attackSpeed = Convert.ToSingle(lineSplit[1]);
+                            dpsData.critChance = Convert.ToSingle(lineSplit[2]);
+                            dpsData.critDamage = Convert.ToSingle(lineSplit[3]);
+                            dpsData.calcDamage = Convert.ToSingle(lineSplit[4]);
+                            dpsData.calcCritDamage = Convert.ToSingle(lineSplit[5]);
+                            dpsData.calcEstimate = Convert.ToSingle(lineSplit[6]);
+
+                            printDPS ();
+                        }
+
+                        f.Close ();
+                        fr.Close ();
+                    }
+                } catch (Exception ex) {
+                    MessageBox.Show ("Could not load save file.\n" + ex.Message);
+                }
+            }
+        }
+        #endregion IO
+    }
+}
